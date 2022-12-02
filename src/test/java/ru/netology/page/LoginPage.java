ebba0d9991fa0370dac0ba5d62dfebee9cbca7b2
MariaG <MGrigorenko@bellintegrator.com>
2022-12-02 23:47:11 +0300

    modified MoneyTransferPage.java

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,10 +1,10 @@
 package ru.netology.page;
 
-
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
@@ -28,7 +28,7 @@ public class LoginPage {
     }
 
     public void findErrorMessage(String expectedText) {
-        errorMessageOfLogin.shouldHave(Condition.exactText(expectedText)).shouldBe(Condition.visible);
+        errorMessageOfLogin.shouldHave(exactText(expectedText)).shouldBe(visible);
     }
 
 }
--- src/test/java/ru/netology/page/MoneyTransferPage.java
+++ src/test/java/ru/netology/page/MoneyTransferPage.java
@@ -2,6 +2,8 @@ package ru.netology.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class MoneyTransferPage {
@@ -9,5 +11,28 @@ public class MoneyTransferPage {
     private SelenideElement from = $("[data-test-id='from'] input");
     private SelenideElement button = $("[data-test-id='action-transfer'] span.button__text");
     private SelenideElement errorMessage = $("[data-test-id='error-notification'] div.notification__content");
+    public DashBoardPage makeValidTransfer(String sum, String cardNumber) {
+        makeTransfer(sum, cardNumber);
+        return new DashBoardPage();
+    }
 
+    public void makeTransfer(String sum, String cardNumber) {
+        count.setValue(sum);
+        from.setValue(cardNumber);
+        button.click();
+    }
+
+    public void makeTransferWithoutSum(String cardNumber) {
+        from.setValue(cardNumber);
+        button.click();
+    }
+
+    public void makeTransferWithoutCardNumber(String sum) {
+        count.setValue(sum);
+        button.click();
+    }
+
+    public void findErrorMessage(String expectedText) {
+        errorMessage.shouldHave(exactText(expectedText)).shouldBe(visible);
+    }
 }
