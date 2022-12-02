852092fc42ef40ae2670123f33a2c3d146c26a28
MariaG <MGrigorenko@bellintegrator.com>
2022-12-02 23:53:25 +0300

    modified VerificationPage.java

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -1,8 +1,10 @@
 package ru.netology.page;
 
-
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
@@ -10,4 +12,22 @@ public class VerificationPage {
     private SelenideElement codeButton = $("[data-test-id='action-verify'] span.button__text");
     private SelenideElement errorMessageOfVerification = $("[data-test-id='error-notification'] div.notification__content");
 
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashBoardPage validVerify(DataHelper.VerificationInfo verificationInfo) {
+        codeField.setValue(verificationInfo.getCode());
+        codeButton.click();
+        return new DashBoardPage();
+    }
+
+    public void invalidVerify(DataHelper.VerificationInfo verificationInfo) {
+        codeField.setValue(verificationInfo.getCode());
+        codeButton.click();
+    }
+
+    public void findErrorMessage(String expectedText) {
+        errorMessageOfVerification.shouldHave(exactText(expectedText)).shouldBe(visible);
+    }
 }
